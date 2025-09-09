66652a862da681105596cc38f4d8ef41c5740023
ProfesorRafaelBiano87 <biano.rafael@escola.pr.gov.br>
2025-09-09 08:30:48 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarTurma(turma) {
+    // Código para mostrar a turma selecionada
+    console.log(`Mostrando turma ${turma}`);
+    // Você pode adicionar mais código aqui para mostrar a turma selecionada
+}
